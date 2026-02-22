82b7b4a021588b74df19b7db15228dbcf9a46acd
Ronald Sandi <A01794620@tec.mx>
2026-02-22 13:48:40 -0600

    refactor (i/o) adding also init's in the check

---
+++ data_handling/__init__.py
@@ -0,0 +1,5 @@
+"""
+This package holds only one handy class,
+the most relevant to file persistence.
+JSON handler package.
+"""
---
+++ setting/__init__.py
@@ -0,0 +1,7 @@
+"""
+Settings.
+
+This package holds two kinds of classes:
+    1. The global setting of the system -aka Setting-.
+    2. Helpers validators - UUI4 and data syntactical checks validator-.
+"""
